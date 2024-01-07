6a4238d3280bad0cee7f2ea20f74c0d663ce0779
shoziking <146511888+shoziking@users.noreply.github.com>
2024-01-07 06:37:56 +0000

    variables datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(" Hello ") //we are using node js not Brower
+
+let name  = " shozab"
+let age = 18
+let isLoggedIn= false
+
+//Numbers
+//bigint
+//String =>" "
+//Boolean => True/False
+//null
+//symbol
+//objects
+console.log(typeof "Shozab");
+
+console.log(typeof null);
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144448
+let accountEmail= "muhammad@gmail.com"
+var accountPassword = "12345"
+accountCity = "D G khan"
+
+//accountId=2
+accountEmail = "shza@gmail.com"
+accountPassword="12124"
+accountCity="lahore"
+ 
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
