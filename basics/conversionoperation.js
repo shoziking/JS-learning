232e97dc555370bd9e285bc5e3ae445397ce048e
shoziking <146511888+shoziking@users.noreply.github.com>
2024-01-07 07:17:49 +0000

    conversionoperations

---
+++ basics/conversionoperation.js
@@ -0,0 +1,47 @@
+// let score = true
+
+// console.log(typeof score)
+
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber )
+// console.log(valueInNumber);
+
+
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1 ; false => 0
+
+
+// let isLoggedIn = 1
+
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+
+// // 1=> true ; 0 => false 
+// // "" => false 
+// // "Shozab" => true 
+
+// let someNumber = 33 
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+
+
+///////////////////////////////////OPERATION//////////////////////////
+let value = 3
+let negValue = -value
+// console.log(negValue);
+ 
+let str1 ="hello"
+let str2 = "shozab"
+let str3 = str1+str2
+console.log(str3);
