0c6865fd834a6d021e518876b74b45c32e39d7d8
shoziking <146511888+shoziking@users.noreply.github.com>
2024-01-07 08:03:54 +0000

    memory understanding

---
+++ basics/memory.js
@@ -0,0 +1,24 @@
+// Stack (Primitive ) ; Heap ( Non Primitive )
+
+let myName = " Shozab"
+
+let anotherName = myName
+
+anotherName= " shozabkhan"
+
+console.log(anotherName);
+console.log(myName);
+
+
+let user = {
+    email: " shozi@gmail.com",
+    paypal : "shozi"
+}
+
+let userTwo =  user
+
+
+userTwo.email=" king@gmail.com"
+
+console.log(user.email);
+console.log(userTwo.email);
